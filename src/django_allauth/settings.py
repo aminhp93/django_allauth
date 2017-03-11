8dfa3b76a3e6ca4f88cb37cb5dd45c8fd2edbf5e
aminhp93 <minhpn.org.ec@gmail.com>
2017-03-10 17:32:40 -0800

    10 - Twitter Authentication

--- src/django_allauth/settings.py
+++ src/django_allauth/settings.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = [
     'allauth.account',
     'allauth.socialaccount',
     'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.twitter',
 
     'crispy_forms',
 
